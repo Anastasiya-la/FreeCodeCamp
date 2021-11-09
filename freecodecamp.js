e2a9dc319c89562ca052cdaae6c78aeb300e9c06
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-09 13:44:43 +0300

    feat: add task

--- freecodecamp.js
+++ freecodecamp.js
@@ -696,4 +696,14 @@ const myDog = {
    
    };
 
+// dot notation
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+  };
+  
+  const hatValue = testObj.hat;      
+  const shirtValue = testObj.shirt;   
+
 
