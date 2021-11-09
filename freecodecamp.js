d5d2987c9bb7864383660b59678ca390c2fb9e4b
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-09 13:45:37 +0300

    feat: add task

--- freecodecamp.js
+++ freecodecamp.js
@@ -706,4 +706,14 @@ const testObj = {
   const hatValue = testObj.hat;      
   const shirtValue = testObj.shirt;   
 
+// bracket notation
+const testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+  };
+  
+  const entreeValue = testObj['an entree'];   
+  const drinkValue = testObj['the drink'];   
+
 
