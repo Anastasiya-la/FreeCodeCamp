354036fe4086982d69b16dc495d4697715edb5d2
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-09 13:40:57 +0300

    feat: add task

--- freecodecamp.js
+++ freecodecamp.js
@@ -563,6 +563,29 @@ function caseInSwitch(val) {
   }
   
   caseInSwitch(1);
+
+  
+// default option in switch
+function switchOfStuff(val) {
+    let answer = "";
+    switch(val){
+      case 'a':
+      answer = 'apple';
+      break;
+      case 'b':
+      answer = 'bird';
+      break;
+      case 'c':
+      answer = 'cat';
+      break;
+      default:
+      answer = 'stuff';
+      break;
+    }
+  
+  
+    return answer;
+  }
   
 
 
