8322a98b9f4506fa4532d2b9df6263d4abc9d761
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-09 18:23:59 +0300

    feat: add task

--- freecodecamp.js
+++ freecodecamp.js
@@ -1072,3 +1072,10 @@ const PI = freezeObj();
 
 //Use Arrow Functions to Write Concise Anonymous Functions
 const magic = () => new Date();
+
+//Write Arrow Functions with Parameters
+const myConcat = (arr1, arr2) => {
+  return arr1.concat(arr2);
+};
+
+console.log(myConcat([1, 2], [3, 4, 5]));
