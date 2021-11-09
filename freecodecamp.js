4323cbb6a2fff86e093b66bb477de1f4ff1a38d0
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-09 13:27:32 +0300

    feat: add task

--- freecodecamp.js
+++ freecodecamp.js
@@ -300,5 +300,18 @@ function processArg(num) {
 }
 processed = processArg(7);
 
+// stand in line
+function nextInLine(arr, item) {
+    arr.push(item);
+   
+    return  arr.shift(0);
+  }
+  
+  const testArr = [1, 2, 3, 4, 5];
+  
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6));
+  console.log("After: " + JSON.stringify(testArr));
+
 
 
