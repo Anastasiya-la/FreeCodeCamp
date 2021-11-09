4d25a2ebbe388588d415ec1778e53455a34be032
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-09 13:40:21 +0300

    feat: add task

--- freecodecamp.js
+++ freecodecamp.js
@@ -542,4 +542,28 @@ if(strokes === 1){
 golfScore(5, 4);
 
 
+// switch
+function caseInSwitch(val) {
+    let answer = "";
+  switch(val){
+   case 1:
+     answer = 'alpha';
+     break;
+   case 2:
+     answer = 'beta';
+     break;
+   case 3:
+     answer =  'gamma';
+     break;
+   case 4:
+     answer = 'delta';
+     break;
+  }
+    return answer;
+  }
+  
+  caseInSwitch(1);
+  
+
+
 
