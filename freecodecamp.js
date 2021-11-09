e3ffedee70e939963b2dc72aa3b7ec9d072f3640
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-09 18:34:23 +0300

    feat: add task

--- freecodecamp.js
+++ freecodecamp.js
@@ -1082,3 +1082,8 @@ console.log(myConcat([1, 2], [3, 4, 5]));
 
 //Set Default Parameters for Your Functions
 const increment = (number, value = 1) => number + value;
+
+//Use the Rest Parameter with Function Parameters
+const sum = (...args) => {
+  return args.reduce((a, b) => a + b, 0);
+}
