08d94ca18d936bc407ed41a08b8c0119dd41d8f0
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-09 13:06:04 +0300

    feat: add task

--- freecodecamp.js
+++ freecodecamp.js
@@ -127,4 +127,7 @@ const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
 // single qoutes 
 const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
 
+// escape 
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; 
+
 
