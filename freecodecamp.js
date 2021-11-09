ea68104b4692b90b3192daabc6a59b63d04f7ba8
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-09 14:00:33 +0300

    feat: add task

--- freecodecamp.js
+++ freecodecamp.js
@@ -1017,4 +1017,15 @@ function countdown(n){
     }
   }
 
+//Use Recursion to Create a Range of Numbe
+function rangeOfNumbers(startNum, endNum) {
+    if (endNum - startNum === 0) {
+       return [startNum];
+   } else {
+       let numbers = rangeOfNumbers(startNum, endNum - 1);
+       numbers. push(endNum);
+       return numbers;
+   }
+   }
+
 
