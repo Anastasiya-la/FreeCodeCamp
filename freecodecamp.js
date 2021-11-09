869bad937266290af7730c85a41be067d3c59b0f
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-09 13:39:11 +0300

    feat: add task

--- freecodecamp.js
+++ freecodecamp.js
@@ -498,5 +498,24 @@ function orderMyLogic(val) {
   
   orderMyLogic(7);
 
+// Chaining If Else
+function testSize(num) {
+    if(num < 5){
+      return 'Tiny';
+    } else if(num < 10){
+      return 'Small';
+    } else if(num < 15){
+      return 'Medium';
+    } else if(num < 20){
+      return 'Large';
+    } else if(num >= 20){
+      return 'Huge';
+    } else {
+      return "Change Me";
+    }
+    }
+    
+    testSize(7);
+
 
 
