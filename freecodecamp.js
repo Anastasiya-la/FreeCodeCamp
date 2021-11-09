acfeda44a0575ecc64cc732ecf1269678bdad322
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-09 13:31:26 +0300

    feat: add tsk

--- freecodecamp.js
+++ freecodecamp.js
@@ -347,5 +347,15 @@ function testStrict(val) {
   
   testStrict(10);
 
+// different values
+function compareEquality(a, b) {
+    if (a === b) { 
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  compareEquality(10, "10");
+
 
 
