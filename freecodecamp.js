fa6088b40e65e0c9b49101a1891ea843ad54708e
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-09 13:49:33 +0300

    feat: add task

--- freecodecamp.js
+++ freecodecamp.js
@@ -780,4 +780,30 @@ function checkObj(obj, checkProp) {
     }
   }
 
+// Manipulating Complex Objects
+const myMusic = [
+    {
+      artist: 'Imagine Dragons',
+      title: 'Natural',
+      release_year: 2018,
+      formats: [
+        'CD',
+        '8T',
+        'LP'
+      ]
+    },
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }
+  ];
+  
+
 
