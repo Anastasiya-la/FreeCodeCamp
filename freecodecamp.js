f8619bba2b0cf8c99a22f4e7b0a55a3915c7e9e7
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-09 13:47:42 +0300

    feat: add task

--- freecodecamp.js
+++ freecodecamp.js
@@ -743,6 +743,15 @@ const myDog = {
     "friends": ["freeCodeCamp Campers"]
   };
   myDog.bark = 'woof';
-  
+
+//delete properties
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+  };
+  delete myDog.tails;
 
 
