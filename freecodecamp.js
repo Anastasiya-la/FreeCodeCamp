053565ccb7edf4b5fd8a54f233938b24aeee7a5b
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-09 13:42:48 +0300

    feat: add task

--- freecodecamp.js
+++ freecodecamp.js
@@ -631,6 +631,14 @@ function chainToSwitch(val) {
     }
     return answer;
   }
+
+  
+//Returning Boolean Values from Functions
+function isLess(a, b) {
+    return a < b;
+  }
+  
+  isLess(10, 15);
   
 
 
