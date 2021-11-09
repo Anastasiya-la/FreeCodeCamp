372a3144dbbf2b6e50899a5beeedf74ea4ec2353
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-09 13:58:59 +0300

    feat: add task

--- freecodecamp.js
+++ freecodecamp.js
@@ -990,4 +990,11 @@ function convertToInteger(str) {
   
   convertToInteger("10011");
 
+//Use the Conditional (Ternary) Operator
+function checkEqual(a, b) {
+    return a === b ? 'Equal' : 'Not Equal';
+  }
+  
+  checkEqual(1, 2);
+
 
