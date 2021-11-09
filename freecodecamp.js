c8bc8eec7c6f7f5196b6a9084b1089431ee895b3
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-09 13:20:41 +0300

    feat: add task

--- freecodecamp.js
+++ freecodecamp.js
@@ -215,5 +215,10 @@ const removedFromMyArray = myArray.pop();
 const myArray = [["John", 23], ["dog", 3]];
 const removedFromMyArray = myArray.shift();
 
+// unshift() 
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+myArray.unshift(['Paul', 35]);
+
 
 
