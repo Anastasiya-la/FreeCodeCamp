553a33f8cd2463aa219339e57ae68fc22bd06626
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-09 13:43:17 +0300

    feat: add task

--- freecodecamp.js
+++ freecodecamp.js
@@ -640,5 +640,16 @@ function isLess(a, b) {
   
   isLess(10, 15);
   
+// return early
+function abTest(a, b) {
+    if (a < 0 || b < 0){
+      return;
+    }
+    
+    
+      return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+    }
+    
+    abTest(2,2);
 
 
