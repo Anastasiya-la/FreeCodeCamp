b114e56a729c63a9b066af5d3571c1ecab6b13ad
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-10 12:29:11 +0300

    feat: add task

--- freecodecamp.js
+++ freecodecamp.js
@@ -1227,3 +1227,9 @@ const thermos = new Thermostat(76); // Setting in Fahrenheit scale
 let temp = thermos.temperature; // 24.44 in Celsius
 thermos.temperature = 26;
 temp = thermos.temperature; // 26 in Celsius
+
+// Create a Module Script
+
+<script type = "module" src="index.js"></script>
+
+
