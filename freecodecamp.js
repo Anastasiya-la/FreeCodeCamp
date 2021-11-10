cdb650c5083695c7faef625f2acbf62377cff0a5
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-10 12:56:30 +0300

    feat: add task

--- freecodecamp.js
+++ freecodecamp.js
@@ -1249,3 +1249,10 @@ import {uppercaseString, lowercaseString} from './string_functions.js';
 uppercaseString("hello");
 lowercaseString("WORLD!");
 
+//Use * to Import Everything from a File
+import * as stringFunctions from './string_functions.js';
+// Only change code above this line
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
+
