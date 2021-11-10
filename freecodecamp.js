d2d5398cef988122c779211ae1106b8292b3b741
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-10 13:13:29 +0300

    feat: add task

--- freecodecamp.js
+++ freecodecamp.js
@@ -1273,3 +1273,15 @@ const makeServerRequest = new Promise((resolve, reject) =>{
 
 });
 
+//Complete a Promise with resolve and reject
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer represents a response from a server
+  let responseFromServer;
+    
+  if(responseFromServer) {
+    resolve('We got the data');// Change this line
+  } else {  
+    reject('Data not received');// Change this line
+  }
+});
+
