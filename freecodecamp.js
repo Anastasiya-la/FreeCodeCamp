2adfb324280b922f36e17a6d17d4c15d95b651e7
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-10 12:53:15 +0300

    feat: add task

--- freecodecamp.js
+++ freecodecamp.js
@@ -1242,3 +1242,10 @@ const lowercaseString = (string) => {
 }
 export {uppercaseString, lowercaseString};
 
+//Reuse JavaScript Code Using import
+import {uppercaseString, lowercaseString} from './string_functions.js';
+// Only change code above this line
+
+uppercaseString("hello");
+lowercaseString("WORLD!");
+
