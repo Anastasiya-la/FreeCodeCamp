4264ea83d74c753c1a37e8767ee576b6f64c7111
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-10 13:04:30 +0300

    feat: add task

--- freecodecamp.js
+++ freecodecamp.js
@@ -1261,3 +1261,9 @@ export default function subtract(x, y) {
   return x - y;
 }
 
+//Import a Default Export
+import subtract from './math_functions.js';
+// Only change code above this line
+
+subtract(7,4);
+
