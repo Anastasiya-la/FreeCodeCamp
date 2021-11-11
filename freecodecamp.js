82d092c16f6a74715887ac284665ae4710477be9
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-11 22:09:53 +0300

    feat: add task

--- freecodecamp.js
+++ freecodecamp.js
@@ -1325,4 +1325,9 @@ let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result = myRegex.test(myString); 
 
+//Match Literal Strings
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; // Change this line
+let result = waldoRegex.test(waldoRegex);
+
 
