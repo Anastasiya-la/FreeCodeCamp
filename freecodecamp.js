c23272a3e5a8f652a433e7f9497185aa6f7732b3
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-11 22:59:24 +0300

    feat: add task

--- freecodecamp.js
+++ freecodecamp.js
@@ -1330,4 +1330,8 @@ let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
 let waldoRegex = /Waldo/; // Change this line
 let result = waldoRegex.test(waldoRegex);
 
+//Match a Literal String with Different Possibilities
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result = petRegex.test(petString);
 
