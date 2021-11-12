5e98c11d466234bfb78d5c94efdf51059ba4b524
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-12 22:53:32 +0300

    feat: add task

--- freecodecamp.js
+++ freecodecamp.js
@@ -1478,3 +1478,18 @@ let result = str.replace(fixRegex, replaceText);
 let hello = "   Hello, World!  ";
 let wsRegex = /^\s+|\s+$/g; // Change this line
 let result = hello.replace(wsRegex, ""); // Change this line
+
+//Use the JavaScript Console to Check the Value of a Variable
+let a = 5;
+let b = 1;
+a++;
+// Only change code below this line
+console.log(a);
+
+let sumAB = a + b;
+console.log(sumAB);
+
+//Understanding the Differences between the freeCodeCamp and Browser Console
+let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+console.log(output);
+console.clear();
