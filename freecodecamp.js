40272c9951645c0c2d9d7cae24cd30a29e738be6
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-12 15:23:36 +0300

    feat: add task

--- freecodecamp.js
+++ freecodecamp.js
@@ -1386,3 +1386,8 @@ let chewieRegex = /Aa*/; // Change this line
 // Only change code above this line
 
 let result = chewieQuote.match(chewieRegex);
+
+//Find Characters with Lazy Matching
+let text = "<h1>Winter is coming</h1>";
+let myRegex = /<h[a-z]*?1>/; // Change this line
+let result = text.match(myRegex);
