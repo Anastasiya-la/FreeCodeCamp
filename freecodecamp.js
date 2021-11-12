6c0cb1db029905b17a68852b1cfb6f98ebb58f62
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-12 23:04:16 +0300

    feat: add task

--- freecodecamp.js
+++ freecodecamp.js
@@ -1500,3 +1500,9 @@ let three = "3";
 console.log(seven + three);
 console.log(typeof seven);
 console.log(typeof three);
+
+//Catch Misspelled Variable and Function Names
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = receivables - payables;
+console.log(`Net working capital is: ${netWorkingCapital}`);
