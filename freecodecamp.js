8f6abf2286cff1afd8bf50978e2d2d814905d994
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-14 23:07:58 +0300

    add task

--- freecodecamp.js
+++ freecodecamp.js
@@ -1670,3 +1670,14 @@ function spreadOut() {
 }
 
 console.log(spreadOut());
+
+//Check For The Presence of an Element With indexOf()
+function quickCheck(arr, elem) {
+  // Only change code below this line
+   if (arr.indexOf(elem) >= 0) {
+    return true;
+  } else {
+    return false;
+  } 
+  // Only change code above this line
+}
