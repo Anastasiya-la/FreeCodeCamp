412d572f074a2270ffd4e49a412ccadaeba17a97
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-14 22:39:52 +0300

    add task

--- freecodecamp.js
+++ freecodecamp.js
@@ -1637,3 +1637,13 @@ function htmlColorNames(arr) {
 }
 
 console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
+
+//Copy Array Items Using slice()
+function forecast(arr) {
+  // Only change code below this line
+ 
+  return arr.slice(2, 4);
+}
+
+// Only change code above this line
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
